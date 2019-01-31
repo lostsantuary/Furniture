3cec91993317c27f4d61cfeac694a68a079a2227
lostsantuary <44250657+lostsantuary@users.noreply.github.com>
2019-01-31 13:49:44 +0800

    Initial commit

---
+++ Assets/Scripts/DoorChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorChange : MonoBehaviour {
+
+    public Sprite[] s1;
+    public Button DButt;
+
+    int count = 0;
+
+    void Awake()
+    {
+        s1 = Resources.LoadAll<Sprite>("Sprites");
+        foreach(Sprite s in s1)
+        {
+            print(s.name);
+        }
+    }
+
+    public void On_Click_Button()
+    {
+        count++;
+
+        if (count == s1.Length)
+        {
+            count = 0;
+        }
+        //print(count);
+        DButt.image.sprite = s1 [count];
+        //print(DButt.image.sprite.name);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WindowChange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WindowChange : MonoBehaviour
+{
+
+    public Sprite[] s1;
+    public Button DButt;
+
+    int count = 0;
+
+    void Awake()
+    {
+        s1 = Resources.LoadAll<Sprite>("SpriteWindow");
+        foreach (Sprite s in s1)
+        {
+            print(s.name);
+        }
+    }
+
+    public void On_Click_Button()
+    {
+        count++;
+
+        if (count == s1.Length)
+        {
+            count = 0;
+        }
+        //print(count);
+        DButt.image.sprite = s1[count];
+        //print(DButt.image.sprite.name);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
